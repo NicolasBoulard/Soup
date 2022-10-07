5f1e6b7faf533cb4701600342fd6466c2943ec02
Nicolas <nicolasboulardpro@gmail.com>
2022-10-07 13:32:53 +0200

    Create model

--- soupui/models.py
+++ soupui/models.py
@@ -2,20 +2,20 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
-class Community(models.Model):
-    pass
-
-
 class OID(models.Model):
     name = models.CharField(max_length=255)
     identifier = models.CharField(max_length=255)
 
-class (models.Model):
-    community = models.ForeignKey(Community, on_delete=models.CASCADE(),)
-    oid = models.M
+
 class Device(models.Model):
     name = models.CharField(max_length=255)
     ip = models.CharField(max_length=255)
     port = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(65535)]
     )
+    community = models.CharField(max_length=255)
+
+
+class Service(models.Model):
+    oid = models.ForeignKey(OID, on_delete=models.CASCADE())
+    device = models.ForeignKey(Device, on_delete=models.CASCADE())
